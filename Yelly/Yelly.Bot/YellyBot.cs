eb82c6212509797bce0f2bbba2e1002eb9ab5f4b
Vamsi TP <vamsitp@microsoft.com>
2018-11-01 21:35:55 +0530

    Yelly.Bot

---
+++ Yelly/Yelly.Bot/YellyBot.cs
@@ -0,0 +1,80 @@
+﻿namespace Yelly.Bot
+{
+    using System;
+    using System.Collections.Concurrent;
+    using System.Threading.Tasks;
+
+    using Microsoft.Bot;
+    using Microsoft.Bot.Builder;
+    using Microsoft.Bot.Connector;
+    using Microsoft.Bot.Connector.Authentication;
+    using Microsoft.Bot.Schema;
+    using Microsoft.Extensions.Configuration;
+
+    public class YellyBot : IBot
+    {
+        private const string DirectLineServiceUrl = "https://directline.botframework.com/";
+        private static readonly ConcurrentDictionary<string, ITurnContext> ChannelActivities = new ConcurrentDictionary<string, ITurnContext>();
+        private readonly ConnectorClient directlineConnector;
+
+        public YellyBot(IConfiguration configuration)
+        {
+            MicrosoftAppCredentials.TrustServiceUrl(DirectLineServiceUrl, DateTime.Now.AddDays(1));
+            var account = new MicrosoftAppCredentials(configuration.GetValue<string>("MicrosoftAppId"), configuration.GetValue<string>("MicrosoftAppPassword"));
+            this.directlineConnector = new ConnectorClient(new Uri(DirectLineServiceUrl), account);
+        }
+
+        public async Task OnTurn(ITurnContext turnContext)
+        {
+            // Respond to the various activity types.
+            switch (turnContext.Activity.Type)
+            {
+                case ActivityTypes.Message:
+                    // Respond to the incoming text message.
+                    await this.RespondToMessage(turnContext);
+                    break;
+
+                case ActivityTypes.ConversationUpdate:
+                    break;
+
+                case ActivityTypes.ContactRelationUpdate:
+                    break;
+
+                case ActivityTypes.Typing:
+                    break;
+
+                case ActivityTypes.DeleteUserData:
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Responds to the incoming message by either sending a hero card, an image,
+        /// or echoing the user's message.
+        /// </summary>
+        /// <param name="context">The context of this conversation.</param>
+        private async Task RespondToMessage(ITurnContext context)
+        {
+            if (context.Activity.ChannelId.Equals("directline"))
+            {
+                ChannelActivities.AddOrUpdate("directLine", context, (key, oldValue) => context);
+            }
+            else
+            {
+                //// var connector = directLine.Services.GetServices<IConnectorClient>().FirstOrDefault().Value;
+                ChannelActivities.TryGetValue("directLine", out var directLine);
+                await this.directlineConnector.Conversations.ReplyToActivityAsync(directLine.Activity.CreateReply(context.Activity.Text));
+            }
+        }
+
+        //// private IMessageActivity GetMessage(ITurnContext context)
+        //// {
+        ////     var message = Activity.CreateMessageActivity();
+        ////     message.Type = ActivityTypes.Message;
+        ////     message.Text = context.Activity.Text;
+        ////     message.From = context.Activity.From;
+        ////     message.Recipient = new ChannelAccount("yelly", "yelly");
+        ////     return message;
+        //// }
+    }
+}
--- Yelly/Yelly/Program.cs
+++ Yelly/Yelly/Program.cs
@@ -2,21 +2,121 @@
 {
     using System;
     using System.Configuration;
+    using System.Diagnostics;
     using System.Globalization;
+    using System.Linq;
     using System.Speech.Synthesis;
+    using System.Threading.Tasks;
+    using Microsoft.Bot.Connector.DirectLine;
+    using Newtonsoft.Json;
 
     class Program
     {
+        private const string YellyStarted = "Yelly started!";
+        private readonly static string BotId = ConfigurationManager.AppSettings["BotId"];
+        private readonly static string DirectLineSecret = ConfigurationManager.AppSettings["DirectLineSecret"];
+        private readonly static string FromUser = ConfigurationManager.AppSettings["BotUser"];
+        private readonly static SpeechSynthesizer Synth = new SpeechSynthesizer();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("\n> Hi, this is Yelly!\n> Type any phrase and hit 'ENTER' to Yell\n> Press 'CTRL + C' to quit\n");
-            using (var synth = new SpeechSynthesizer())
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("\n> Hi, this is Yelly!\n> Type any phrase and hit 'ENTER' to Yell\n> Press 'CTRL + C' or type 'EXIT' and hit 'ENTER' to quit\n");
+            Console.BackgroundColor = ConsoleColor.Black;
+            SetSpeech(Synth);
+            StartBotConversation().GetAwaiter().GetResult();
+        }
+
+        private static async Task StartBotConversation()
+        {
+            // Create a new Direct Line client.
+            var client = new DirectLineClient(DirectLineSecret);
+
+            // Start the conversation.
+            var conversation = await client.Conversations.StartConversationAsync();
+
+            // Start the bot message reader in a separate thread.
+            new System.Threading.Thread(async () => await ReadBotMessagesAsync(client, conversation.ConversationId)).Start();
+
+            await SendMessage(client, conversation, YellyStarted);
+
+            // Loop until the user chooses to exit this loop.
+            while (true)
             {
-                SetSpeech(synth);
-                string input;
-                while ((input = Console.ReadLine()) != null)
+                // Accept the input from the user.
+                string input = Console.ReadLine().Trim();
+
+                // Check to see if the user wants to exit.
+                if (input.Trim().Equals("exit", StringComparison.OrdinalIgnoreCase))
                 {
-                    synth.Speak(input);
+                    // Exit the app if the user requests it.
+                    break;
+                }
+                else
+                {
+                    Synth.SpeakAsync(input);
+                }
+            }
+        }
+
+        private static Task SendMessage(DirectLineClient client, Conversation conversation, string input)
+        {
+            // Create a message activity with the text the user entered.
+            var userMessage = new Activity
+            {
+                From = new ChannelAccount(FromUser, FromUser),
+                Text = input,
+                Type = ActivityTypes.Message
+            };
+
+            // Send the message activity to the bot.
+            return client.Conversations.PostActivityAsync(conversation.ConversationId, userMessage);
+        }
+
+
+        /// <summary>
+        /// Polls the bot continuously and retrieves messages sent by the bot to the client.
+        /// </summary>
+        /// <param name="client">The Direct Line client.</param>
+        /// <param name="conversationId">The conversation ID.</param>
+        /// <returns></returns>
+        private static async Task ReadBotMessagesAsync(DirectLineClient client, string conversationId)
+        {
+            string watermark = null;
+
+            // Poll the bot for replies once per second.
+            while (true)
+            {
+                // Retrieve the activity set from the bot.
+                try
+                {
+                    var activitySet = await client.Conversations.GetActivitiesAsync(conversationId, watermark);
+                    watermark = activitySet?.Watermark;
+
+                    // Extract the activities sent from our bot.
+                    var activities = from x in activitySet.Activities
+                                     where x.From.Id == BotId
+                                     select x;
+
+                    // Analyze each activity in the activity set.
+                    foreach (var activity in activities)
+                    {
+                        var message = activity.Text;
+                        if (!message.Equals(YellyStarted))
+                        {
+                            Console.BackgroundColor = ConsoleColor.DarkCyan;
+                            Console.WriteLine(message);
+                            Console.BackgroundColor = ConsoleColor.Black;
+                            Synth.SpeakAsync(message);
+                        }
+                    }
+
+                    // Wait for one second before polling the bot again.
+                    await Task.Delay(TimeSpan.FromSeconds(1)).ConfigureAwait(false);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
                 }
             }
         }
