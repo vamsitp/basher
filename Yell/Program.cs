61ccc01d18bff98ea63cde1053883b9eae493590
Vamsi TP <vamsitp@microsoft.com>
2018-11-01 12:24:56 +0530

    Added Yell app and upgraded NUGETs.

---
+++ Yell/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Yell
+{
+    using System;
+    using System.Configuration;
+    using System.Globalization;
+    using System.Speech.Synthesis;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n> Type any phrase and hit 'ENTER' to Yell\n> Press 'CTRL + C' to quit\n");
+            using (var synth = new SpeechSynthesizer())
+            {
+                SetSpeech(synth);
+                string input;
+                while ((input = Console.ReadLine()) != null)
+                {
+                    synth.Speak(input);
+                }
+            }
+        }
+
+        private static void SetSpeech(SpeechSynthesizer synth)
+        {
+            synth.SetOutputToDefaultAudioDevice();
+            Enum.TryParse<VoiceGender>(ConfigurationManager.AppSettings["VoiceGender"], true, out var voiceGender);
+            Enum.TryParse<VoiceAge>(ConfigurationManager.AppSettings["VoiceAge"], true, out var voiceAge);
+            int.TryParse(ConfigurationManager.AppSettings["VoiceAlternate"], out var voiceAlternate);
+            synth.SelectVoiceByHints(voiceGender, voiceAge, voiceAlternate, CultureInfo.GetCultureInfo(ConfigurationManager.AppSettings["CultureInfo"]));
+        }
+    }
+}
