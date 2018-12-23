57b839a6a00aef62ab458ca6845f97551a654827
Vamsi TP <vamsitp@microsoft.com>
2018-12-23 18:25:33 +0530

    Options class instead of raw-parsing

---
+++ Burner/Options.cs
@@ -0,0 +1,107 @@
+﻿namespace Burner
+{
+    using System.Collections.Generic;
+
+    using CommandLine;
+    using CommandLine.Text;
+
+    public class Options
+    {
+        ////[Option('v', "verbose", Required = false, HelpText = "Prints all messages to standard output.")]
+        ////public bool Verbose { get; set; }
+    }
+
+    [Verb("setup", HelpText = "Set default Azure DevOps account (-a <accountName> -p <projectName>) -t <pat>")]
+    class SetupOptions : Options
+    {
+        [Option('a', "account", Required = true, HelpText = "Azure DevOps Account/Org")]
+        public string Account { get; set; }
+
+        [Option('p', "project", Required = false, HelpText = "Azure DevOps Project")]
+        public string Project { get; set; }
+
+        [Option('t', "token", Required = false, HelpText = "Azure DevOps PAT (Token)")]
+        public string Token { get; set; }
+
+        [Usage(ApplicationAlias = "burner")]
+
+        public static IEnumerable<Example> Examples
+        {
+            get
+            {
+                yield return new Example("\n\nSet Azure DevOps Account", new SetupOptions { Account = "vamsitp", Project = "VstsDemoGenerator", Token = "ds2f3m35a56s7i78l8w8efksjdcbsklfhwuie" });
+            }
+        }
+    }
+
+    [Verb("update", HelpText = "Update Work-item (-i <id> -c <completedWork>) -r <remainingWork>)")]
+    class UpdateItemOptions : Options
+    {
+        [Option('i', "id", Required = true, HelpText = "ID")]
+        public int Id { get; set; }
+
+        [Option('c', "completed", Required = true, HelpText = "Completed Work")]
+        public int CompletedWork { get; set; }
+
+        [Option('r', "remaining", Required = true, HelpText = "Remaining Work")]
+        public int RemainingWork { get; set; }
+
+        [Usage(ApplicationAlias = "burner")]
+
+        public static IEnumerable<Example> Examples
+        {
+            get
+            {
+                yield return new Example("\nUpdate Work-item", UnParserSettings.WithGroupSwitchesOnly(), new UpdateItemOptions { Id = 213, CompletedWork = 4, RemainingWork = 12 });
+            }
+        }
+    }
+
+    [Verb("items", HelpText = "Get User-assignments (-u <space-delimited aliass>)")]
+    class UserAssignmentOptions : Options
+    {
+        [Option('u', "users", Required = true, HelpText = "Users")]
+        public IEnumerable<string> Users { get; set; }
+
+        [Usage(ApplicationAlias = "burner")]
+
+        public static IEnumerable<Example> Examples
+        {
+            get
+            {
+                yield return new Example("\nGet Work-items for Users", UnParserSettings.WithGroupSwitchesOnly(), new UserAssignmentOptions { Users = new[] { "vamsitp", "someone@outlook.com" } });
+            }
+        }
+    }
+
+    [Verb("details", HelpText = "Get Work-item details (-i <ids>)")]
+    class DetailsOptions : Options
+    {
+        [Option('i', "ids", Required = true, HelpText = "IDs")]
+        public IEnumerable<string> Ids { get; set; }
+
+        [Usage(ApplicationAlias = "burner")]
+
+        public static IEnumerable<Example> Examples
+        {
+            get
+            {
+                yield return new Example("\nGet Item Details", UnParserSettings.WithGroupSwitchesOnly(), new DetailsOptions { Ids = new[] { "213", "546" } });
+            }
+        }
+    }
+
+    [Verb("clear", HelpText = "Clear account settings")]
+    class ClearSettingsOptions : Options
+    {
+        [Usage(ApplicationAlias = "burner")]
+
+        public static IEnumerable<Example> Examples
+        {
+            get
+            {
+                yield return new Example("\nClear account settings", UnParserSettings.WithGroupSwitchesOnly(), new ClearSettingsOptions());
+            }
+        }
+    }
+}
--- Burner/Program.cs
+++ Burner/Program.cs
@@ -13,6 +13,8 @@
 
     using ColoredConsole;
 
+    using CommandLine;
+
     using Serilog;
 
     public class Program
@@ -20,32 +22,118 @@
         private const string DateFormat = "dd.MMM.yy";
         private const string SpacesPrefix = "         ";
 
-        private static readonly NameValueCollection Commands = ConfigurationManager.GetSection("commands") as NameValueCollection;
-
         public static List<AzureDevOpsUser> Users { get; private set; }
 
         private static void Main(string[] args)
         {
             var logPath = Path.Combine("./Logs", "Burner_{Date}.log");
             Log.Logger = new LoggerConfiguration().MinimumLevel.Warning().WriteTo.RollingFile(logPath, outputTemplate: "{Timestamp:dd-MMM-yyyy HH:mm:ss} | [{Level:u3}] {Message}{NewLine}{Exception}").Enrich.FromLogContext().CreateLogger();
-            CheckSettings().GetAwaiter().GetResult();
-            Execute(args).GetAwaiter().GetResult();
-            Main(Console.ReadLine().Split(' '));
+            while (true)
+            {
+                var result = Parser.Default
+                            .ParseArguments<SetupOptions, UserAssignmentOptions, UpdateItemOptions, ClearSettingsOptions>(args)
+                            .MapResult(
+                            (SetupOptions opts) => SetConfig(opts.Account, opts.Project, opts.Token),
+                            (UserAssignmentOptions opts) => GetUserAssignments(opts.Users).GetAwaiter().GetResult(),
+                            (UpdateItemOptions opts) => UpdateItem(opts.Id, opts.CompletedWork, opts.RemainingWork).GetAwaiter().GetResult(),
+                            (ClearSettingsOptions opts) => SetConfig(string.Empty, string.Empty, string.Empty),
+                            errs => HandleParseErrors(errs?.ToList()));
+
+                Log.Information("Result: " + result);
+                args = Console.ReadLine().Trim().Split(' ');
+            }
+        }
+
+        private static async Task<int> GetUserAssignments(IEnumerable<string> users)
+        {
+            await CheckSettings();
+            if (users?.Any() == true)
+            {
+                foreach (var u in users)
+                {
+                    try
+                    {
+                        var user = Users?.FirstOrDefault(x => (u?.Contains("@") == true && x?.PrincipalName?.Equals(u, StringComparison.OrdinalIgnoreCase) == true) || ((u?.Contains("@") != true) && x?.PrincipalName?.Split('@')?.FirstOrDefault()?.Equals(u, StringComparison.OrdinalIgnoreCase) == true))?.PrincipalName ?? u;
+                        var workItems = await AzureDevOps.GetWorkItems(user);
+                        var bugs = workItems?.Count(x => x.Fields.WorkItemType.Equals("Bug")).ToString();
+                        var tasks = workItems?.Count(x => x.Fields.WorkItemType.Equals("Task")).ToString();
+                        ColorConsole.WriteLine($"\n{workItems?.FirstOrDefault()?.Fields?.AssignedTo.ToUpperInvariant() ?? user.ToUpperInvariant()}: {workItems?.Count}".Cyan(), " (", "B: ", $" {bugs ?? string.Empty} ".White().OnRed(), " / T: ", $" {tasks ?? string.Empty} ".Black().OnYellow(), ")\n");
+                        var i = 0;
+                        workItems?.ForEach(wi =>
+                        {
+                            i++;
+                            var createdDate = wi.Fields.CreatedDate.ToString(DateFormat);
+                            var elapsed = new StringBuilder($" {createdDate} ");
+                            for (var e = 1; e < DateTimeOffset.Now.Subtract(wi.Fields.CreatedDate).Days - createdDate.Length; e++)
+                            {
+                                elapsed.Append(" ");
+                            }
+
+                            var completed = $" {wi.Fields.CompletedWork} (C) ".White().OnDarkGreen();
+                            var remaining = $" {wi.Fields.RemainingWork} (R) ".White().OnDarkRed();
+                            var original = $" {wi.Fields.OriginalEstimate} (O) ".White().OnDarkGray();
+
+                            var severity = $" {wi.Fields.Severity} ".White().OnDarkMagenta();
+                            var priority = $" P{wi.Fields.Priority} ".White().OnDarkCyan();
+
+                            var index = i.ToString().PadLeft(2, '0');
+                            var wiType = $"[{index}.{wi.Fields.WorkItemType.FirstOrDefault()}]";
+                            ColorConsole.WriteLine($"  ", wiType.EndsWith("B]", StringComparison.OrdinalIgnoreCase) ? wiType.Red() : wiType.Yellow(), " ", $" {wi.Id} ".Black().OnWhite(), $" {wi.Fields.Title}");
+                            ColorConsole.WriteLine(SpacesPrefix, elapsed.ToString().OnRed());
+                            ColorConsole.WriteLine(SpacesPrefix, severity, " / ", priority);
+                            ColorConsole.WriteLine(SpacesPrefix, completed, " + ", remaining, " / ", original);
+                            Console.WriteLine();
+                        });
+                    }
+                    catch (Exception ex)
+                    {
+                        Log.Error(ex, "Invalid Command");
+                        ColorConsole.WriteLine("Invalid Command: " + ex.Message?.Red() ?? string.Empty);
+                    }
+                }
+            }
+
+            return 0;
+        }
+
+        private static async Task<int> UpdateItem(int id, float completedWork, float remainingWork)
+        {
+            await CheckSettings();
+            var update = await AzureDevOps.SaveWorkItemsAsync(new WorkItem { Id = id, Fields = new Fields { CompletedWork = completedWork, RemainingWork = remainingWork } });
+            ColorConsole.WriteLine(update ? $"{id} updated! ".Green() : $" Could not update {id}! ".Red());
+            return 0;
+        }
+
+        private static int HandleParseErrors(List<Error> errs)
+        {
+            if (errs.Count > 1)
+            {
+                errs.ToList().ForEach(e => Log.Error(e.ToString()));
+            }
+
+            return errs.Count;
         }
 
         private static async Task CheckSettings()
         {
             if (string.IsNullOrWhiteSpace(AzureDevOps.Account) || string.IsNullOrWhiteSpace(AzureDevOps.Project) || string.IsNullOrWhiteSpace(AzureDevOps.Token))
             {
-                ColorConsole.WriteLine("\n Please provide Azure DevOps details in the format (without braces): <Account> <Project> <PersonalAccessToken> ".Black().OnCyan());
+                ColorConsole.WriteLine("\n Please provide Azure DevOps details using the 'setup' option".Black().OnCyan());
                 var details = Console.ReadLine().Split(' ');
-                SetConfig(details);
+                Parser.Default
+                            .ParseArguments<SetupOptions>(details)
+                            .MapResult(
+                            (SetupOptions opts) => SetConfig(opts.Account, opts.Project, opts.Token),
+                            errs => HandleParseErrors(errs?.ToList()));
             }
 
-            Users = await AzureDevOps.GetUsers().ConfigureAwait(false);
+            if (Users == null || Users.Count == 0)
+            {
+                Users = await AzureDevOps.GetUsers().ConfigureAwait(false);
+            }
         }
 
-        private static void SetConfig(params string[] details)
+        private static int SetConfig(params string[] details)
         {
             var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
             var section = config.GetSection("appSettings") as AppSettingsSection;
@@ -59,86 +147,8 @@
 
             config.Save(ConfigurationSaveMode.Minimal);
             ConfigurationManager.RefreshSection(section.SectionInformation.Name);
-        }
-
-        private static async Task Execute(string[] args)
-        {
-            var command = Commands.AllKeys.SingleOrDefault(x => args.Length > 0 && Commands[x].StartsWith(args?.FirstOrDefault() ?? string.Empty, StringComparison.OrdinalIgnoreCase));
-            if (command != null)
-            {
-                if (args[0].StartsWith("-u", StringComparison.OrdinalIgnoreCase))
-                {
-                    var update = await AzureDevOps.SaveWorkItemsAsync(new WorkItem { Id = int.Parse(args[1]), Fields = new Fields { CompletedWork = float.Parse(args[3]), RemainingWork = float.Parse(args[5]) } });
-                    ColorConsole.WriteLine(update ? $"{args[1]} updated! ".Green() : $" Could not update {args[1]}! ".Red());
-                    await ReExecute();
-                }
-                else if (args[0].StartsWith("-c", StringComparison.OrdinalIgnoreCase))
-                {
-                    SetConfig(string.Empty, string.Empty, string.Empty);
-                    await CheckSettings();
-                    await ReExecute();
-                }
-            }
-            else
-            {
-                var users = args.SelectMany(a => a.Split(new[] { ',', ';' }, StringSplitOptions.RemoveEmptyEntries)?.Select(x => x.Trim()))?.ToList();
-                if (users?.Count > 0)
-                {
-                    foreach (var u in users)
-                    {
-                        try
-                        {
-                            var user = Users.FirstOrDefault(x => (u.Contains("@") && x.PrincipalName.Equals(u, StringComparison.OrdinalIgnoreCase)) || (!u.Contains("@") && x.PrincipalName.Split('@').FirstOrDefault().Equals(u, StringComparison.OrdinalIgnoreCase))).PrincipalName;
-                            var workItems = await AzureDevOps.GetWorkItems(user);
-                            var bugs = workItems?.Count(x => x.Fields.WorkItemType.Equals("Bug")).ToString();
-                            var tasks = workItems?.Count(x => x.Fields.WorkItemType.Equals("Task")).ToString();
-                            ColorConsole.WriteLine($"\n{workItems?.FirstOrDefault()?.Fields?.AssignedTo.ToUpperInvariant() ?? user.ToUpperInvariant()}: {workItems?.Count}".Cyan(), " (", "B: ", $" {bugs ?? string.Empty} ".White().OnRed(), " / T: ", $" {tasks ?? string.Empty} ".Black().OnYellow(), ")\n");
-                            var i = 0;
-                            workItems?.ForEach(wi =>
-                            {
-                                i++;
-                                var createdDate = wi.Fields.CreatedDate.ToString(DateFormat);
-                                var elapsed = new StringBuilder($" {createdDate} ");
-                                for (var e = 1; e < DateTimeOffset.Now.Subtract(wi.Fields.CreatedDate).Days - createdDate.Length; e++)
-                                {
-                                    elapsed.Append(" ");
-                                }
-
-                                var completed = $" {wi.Fields.CompletedWork} (C) ".White().OnDarkGreen();
-                                var remaining = $" {wi.Fields.RemainingWork} (R) ".White().OnDarkRed();
-                                var original = $" {wi.Fields.OriginalEstimate} (O) ".White().OnDarkGray();
-
-                                var severity = $" {wi.Fields.Severity} ".White().OnDarkMagenta();
-                                var priority = $" P{wi.Fields.Priority} ".White().OnDarkCyan();
-
-                                var index = i.ToString().PadLeft(2, '0');
-                                var wiType = $"[{index}.{wi.Fields.WorkItemType.FirstOrDefault()}]";
-                                ColorConsole.WriteLine($"  ", wiType.EndsWith("B]", StringComparison.OrdinalIgnoreCase) ? wiType.Red() : wiType.Yellow(), " ", $" {wi.Id} ".Black().OnWhite(), $" {wi.Fields.Title}");
-                                ColorConsole.WriteLine(SpacesPrefix, elapsed.ToString().OnRed());
-                                ColorConsole.WriteLine(SpacesPrefix, severity, " / ", priority);
-                                ColorConsole.WriteLine(SpacesPrefix, completed, " + ", remaining, " / ", original);
-                                Console.WriteLine();
-                            });
-                        }
-                        catch (Exception ex)
-                        {
-                            Log.Error(ex, "Invalid Command");
-                            ColorConsole.WriteLine("Invalid Command: " + ex.Message?.Red() ?? string.Empty);
-                        }
-                    }
-                }
-                else
-                {
-                    await ReExecute();
-                }
-            }
-        }
-
-        private static Task ReExecute()
-        {
-            ColorConsole.WriteLine("\n Provide a list of aliases to track (e.g.: vamsitp | vamsitp@outlook.com ) or a Command to execute (e.g.: -update 278 c 10 r 3 | -clear)".White().OnRed());
-            var details = Console.ReadLine().Split(' ');
-            return Execute(details);
+            Users = null;
+            return 0;
         }
     }
 }
